d700ead0fa8b8a13ec1287d3777fd8bef5bb7442
SophieNystuen <sophienystuen@brandeis.edu>
2025-03-16 17:29:54 -0400

    Update sample_data.py

--- mock_db/sample_data.py
+++ mock_db/sample_data.py
@@ -23,3 +23,40 @@ schedule = {
         ]
     }
 }
+
+    child = { 
+    "child_id_1": { 
+        "name" : "joe" 
+        "age" : "10" 
+        "sex" : "male" 
+        "intrests" : "playing tennis"   
+    }
+
+}
+
+    FAQ = {
+    "FAQ_id_1": { 
+        "where is the 1st aid box?" : "cabinet in the 1st floor bathroom" 
+        "where are the emergency contacts?" : "On the fridge"
+        "what can the kid watch?" : " movie [netflix, disney]" 
+        " can '' get a treat?" : "After they are done with their must do items[piano practice, math hw]" 
+                 }
+
+        }
+    rules = { 
+        "rules_id_1": {
+            "rule 1" : "vegetarian diet only - no meat and no eggs"
+            "rule 2" : "we tidy up after playtime. Put away toys and books after use." 
+            "rule 3": "be kind to each other. No hitting, pushing, or hurting others."
+            "rule 4": "use nice words. speak politely and respectfully to everyone in the house"
+            "rule 5" : "screen time. no screens during meals and no screens after 8:00 P.M."
+            "rule 6": "book time begins at 7:00 PM" 
+            "rule 7": "tell a grownup if you need help or feel scared."
+            }
+
+        }
+
+
+            
+            
+    
