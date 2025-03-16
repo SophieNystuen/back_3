c679512fa2ce214b49c3ba784d37b6a18f2efc1a
Allen <allensnewton@gmail.com>
2025-03-16 15:25:02 -0400

    Add prelim setup

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-git remote add origin https://github.com/SophieNystuen/back_3.git# This is a sample Python script.
+from flask import Flask
+app = Flask(__name__)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@app.route('/schedule', methods = ['GET'])
+def get_schedule():
+   return 'Hello GET'
 
+@app.route('/schedule', methods = ['POST'])
+def post_schedule():
+   return 'Hello POST'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/')
+def hello_world():
+   return 'Hello World'
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+   app.run()
